7328ab29c4064dc8adff22a9f2fa4a751ace8a42
Afraz Hussain Syed <syedafrazofficial@gmail.com>
2025-03-22 03:38:59 -0500

    Fixed the typo in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema(
     {
         _id: {type: String, required: true},
-        Name: {type: String, required: true},
+        name: {type: String, required: true},
         email: {type: String, required: true},
         imageUrl: {type: String, required: true},
         enrolledCourses : [
